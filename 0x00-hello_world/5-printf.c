5a5f9fed955133ffefd3c4920eb099ea976121b6
Oyelekankehinde <oyelekankehinde2@gmail.com>
2023-03-17 01:30:35 +0100

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#iclude <stdio.h>
+/**
+ * main - print a c program u printf
+ *
+ * Return: 0
+ */
+int main(viod)
+{
+        printf("with proper grammar, but the outcome is a piece of art,"\n");
+}	
